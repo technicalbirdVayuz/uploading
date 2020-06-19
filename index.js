60517a46dd57e7b97bb4fabc12ee4e2880c22128
mixomatric <ankit.vayuz@gmail.com>
2020-06-19 15:56:40 +0530

    Adding Limit

--- index.js
+++ index.js
@@ -1,61 +1,45 @@
-var app = require('express')();
-var bodyParser = require('body-parser')
-var express = require('express');
+const express = require('express');
+const fileUpload = require('express-fileupload');
 
-var fs = require('fs'),
-    path = require('path'),
-    Handlebars = require('handlebars');
-var nodemailer = require('nodemailer');
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
-app.use(bodyParser.json())
-app.use("/public", express.static(path.join(__dirname, 'public')));
-app.use(function(req,res,next){
-        res.setHeader('Access-Control-Allow-Origin', "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With, Origin, Content-Type, Accept");
-        res.header("Access-Control-Allow-Methods", "GET, POST, PUT");
-        res.header("Access-Control-Allow-Credentials", true);
-        res.header("X-Frame-Options", "DENY");
-        res.header("accept-encoding", "gzip,deflate");
-        res.header("Content-Security-Policy", "frame-ancestors 'none'");
-        res.header("X-XSS-Protection", "1; mode=block");
-        next();
- });
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var Fiber = require('fibers');
-var database = require("./database.js");
-var bodyParser = require('body-parser')
-var md5 = require('md5');
+const app = express();
+const path = require('path');
 
-var Chatroom = require('./database.js').Chatroom;
-var UserInfo = require('./database.js').UserInfo;
-var ChatroomLogs = require('./database.js').ChatroomLogs;
-var BlockedUsers = require('./database.js').BlockedUsers;
-const PORT = 8080;
-app.get('/', function(req, res) {
-  return res.status(200).send({"message":"Empty","status":200});  
+const PORT = 3000;
+app.use('/form', express.static(__dirname + '/index.html'));
+app.use("/uploads", express.static(path.join(__dirname, 'uploads')));
+// default options
+app.use(fileUpload({
+	  limits: { fileSize: 50 * 1024 * 1024 },
+}));
+
+app.get('/ping', function(req, res) {
+  res.send('pong');
 });
 
+app.post('/upload', function(req, res) {
+  let sampleFile;
+  let uploadPath;
 
-app.post("/upload_image_in_base64",function(req, res) {
- var base64str = req.body.target_image;
+  if (!req.files || Object.keys(req.files).length === 0) {
+    res.status(400).send('No files were uploaded.');
+    return;
+  }
 
- var buf = Buffer.from(base64str,'base64');
- var fileName ="file_" + new Date().getTime();
- fs.writeFile(path.join(__dirname,'/public/',fileName), buf, function(error){
-      if(error){
-          console.log(error);
-          throw error;
-       }else{
-           return res.status(200).send({"message":"Image Uploaded Successfully","result":"https://messages.ce5contact.com/" + "public/" + fileName,"status":200});  
-       }
-      }); 
+  console.log('req.files >>>', req.files); // eslint-disable-line
+
+  sampleFile = req.files.sampleFile;
+
+  uploadPath = __dirname + '/uploads/' + sampleFile.name;
+
+  sampleFile.mv(uploadPath, function(err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+
+    res.send('File uploaded to ' + uploadPath);
+  });
 });
 
-// Start server
-try {
-    http.listen(process.env.PORT || PORT);
-} catch (e) {
-    console.error(e);
-}
+app.listen(PORT, function() {
+  console.log('Express server listening on port ', PORT); // eslint-disable-line
+});
